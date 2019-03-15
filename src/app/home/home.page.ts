2432d0e088d86984acd16ad9fbd4fb43adee0222
Jonathan Smith <Jonathan_Smith@waters.com>
2019-03-15 14:18:51 +0000

    set up basic demo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Driver from 'driver.js';
+import 'driver.js/dist/driver.min.css';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
+  driver: Driver;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.driver = new Driver();
+  }
+
+  enableDriver() {
+    this.driver.highlight('#my-element');
+  }
 }
